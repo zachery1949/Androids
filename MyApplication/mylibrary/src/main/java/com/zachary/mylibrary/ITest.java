c955ac7edd8753328cef2570f8ac536a4c979673
zhengchao@llvision.com <zhengchao@llvision.com>
2023-02-15 12:47:49 +0800

    【功能】打包成jar包和使用jar包

---
+++ MyApplication/mylibrary/src/main/java/com/zachary/mylibrary/ITest.java
@@ -0,0 +1,12 @@
+package com.zachary.mylibrary;
+
+import android.util.Log;
+
+public class ITest {
+    public void testPrint(){
+        System.out.println("zhangsan：");
+        Log.d("TAG", "testPrint: ");
+    }
+}
+
+
